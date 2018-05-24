a6cba21527cb3e5b6ebca94d1725b47f44030fdf
Desarrollo <desarrollo@ondata.es>
2018-05-24 13:53:41 +0200

    Objeto persona

---
+++ funciones.js
@@ -0,0 +1,12 @@
+var person = {'nombre':'Luis Alberto',
+                'Apellidos':'Gazquez'};
+
+function miAlerta()
+{
+    try 
+    {
+        alert(person.nombre);        
+    } catch (error) {
+        
+    }
+}
